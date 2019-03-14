3fcf67b7b304e5e41cf847af0dc92321e84b7c43
lianchao <lianchao@xueliman.com>
2019-03-14 09:09:00 +0800

    ok

--- LargeIntegerMultiplication/index.js
+++ LargeIntegerMultiplication/index.js
@@ -1,1 +1,32 @@
-var a = 0;
+var LIMultiplication = /** @class */ (function () {
+    function LIMultiplication(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.isPositive = true;
+    }
+    LIMultiplication.prototype.normalMul = function () {
+        return this.num1 * this.num2;
+    };
+    LIMultiplication.prototype.lIMul = function () {
+        var isPositive = 1;
+        if ((this.num1 < 0 && this.num2 > 0) || (this.num1 > 0 && this.num2 < 0)) {
+            isPositive = -1;
+        }
+        return isPositive * this.mul(Math.abs(this.num1), Math.abs(this.num2));
+    };
+    LIMultiplication.prototype.mul = function (num1, num2) {
+        var len1 = (num1 + '').length;
+        var len2 = (num2 + '').length;
+        if (len1 > 1 && len2 > 1) {
+            var mul1 = Math.floor(len1 / 2);
+            var mul2 = Math.floor(len2 / 2);
+            var A = Math.floor(num1 / Math.pow(10, mul1));
+            var B = num1 % Math.pow(10, mul1);
+            var C = Math.floor(num2 / Math.pow(10, mul2));
+            var D = num2 % Math.pow(10, mul2);
+            return this.mul(A, C) * Math.pow(10, mul1 + mul2) + this.mul(A, D) * Math.pow(10, mul1) + this.mul(B, C) * Math.pow(10, mul2) + this.mul(B, D);
+        }
+        return num1 * num2;
+    };
+    return LIMultiplication;
+}());
--- LargeIntegerMultiplication/index.ts
+++ LargeIntegerMultiplication/index.ts
@@ -1,34 +1,34 @@
-class LIM {
+class LIMultiplication {
     num1: number;
     num2: number;
+    isPositive: boolean;
     constructor(num1: number, num2: number) {
         this.num1 = num1
         this.num2 = num2
+        this.isPositive = true
     }
     normalMul(): number {
         return this.num1 * this.num2
     }
     lIMul(): number {
-        return this.mul(this.num1, this.num2)
+        let isPositive = 1
+        if((this.num1 < 0 && this.num2 > 0) || (this.num1 > 0 && this.num2 < 0)) {
+            isPositive = -1
+        }
+        return isPositive * this.mul(Math.abs(this.num1), Math.abs(this.num2))
     }
     private mul(num1: number, num2: number): number {
         let len1 = (num1 + '').length
         let len2 = (num2 + '').length
-        if(num1 === 0 || num2 === 0) {
-            return 0
-        } else if(len1 > 1) {
-            if(len2 > 1) {
-
-            } else {
-
-            }
-        } else {
-            if(len2 > 1) {
-
-            } else {
-
-            }
+        if(len1 > 1 && len2 > 1) {
+            let mul1 = Math.floor(len1 / 2)
+            let mul2 = Math.floor(len2 / 2)
+            let A = Math.floor(num1 / Math.pow(10, mul1))
+            let B = num1 % Math.pow(10, mul1)
+            let C = Math.floor(num2 / Math.pow(10, mul2))
+            let D = num2 % Math.pow(10, mul2)        
+            return this.mul(A, C) * Math.pow(10, mul1 + mul2) + this.mul(A, D) * Math.pow(10, mul1) + this.mul(B, C) * Math.pow(10, mul2) + this.mul(B, D)
         }
-        return 1
+        return num1 * num2
     }
 }
